a945651e92eddb9da79f9772100e606b04b4fb6d
valmack <8445610+valmack@users.noreply.github.com>
2021-01-10 14:32:29 -0500

    fix: pass array into contract installer

--- src/bin/godmode.ts
+++ src/bin/godmode.ts
@@ -4,16 +4,21 @@ import { installer } from '@xgm/contract-library';
 
 import program from 'commander';
 
+interface InstallOptions {
+    all: boolean;
+    contracts: string;
+}
+
 program
     .command('install')
     .option('-a, --all', 'Installs all GM contracts')
-    .option('--contracts <array>', 'List of GM contract names')
+    .option('--contracts <list>', 'Space-separated list of GM contract names')
     .description('Install GM smart contracts')
-    .action(async ({ all, contracts }) => {
+    .action(async ({ all, contracts }: InstallOptions) => {
         if (all) {
             await installer();
         } else if (contracts) {
-            await installer(contracts);
+            await installer(contracts.split(' '));
         } else {
             const projectPackageJsonPath = path.join(
                 process.cwd(),
