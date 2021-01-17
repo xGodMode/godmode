a9540350008faeb3d540fd8485eb9a0f07f440d5
valmack <8445610+valmack@users.noreply.github.com>
2021-01-17 16:48:47 -0500

    feat: test execute and unlock account without password

--- test/core/gm.test.ts
+++ test/core/gm.test.ts
@@ -1,11 +1,18 @@
 import ganache from 'ganache-core';
+import Web3 from 'web3';
 
 import chai from '../utils/chai';
 import GMDep = require('../../src/gm.dep');
 import { GM } from '../../src/gm';
+import contracts from '../../src/common/contracts';
 import { GMError } from '../../src/common/errors';
-import Web3 from 'web3';
 import { accountIsLocked } from '../utils/eth';
+
+const { GMDai } = contracts;
+const GMDaiAbi = GMDai.contracts['GMDai.sol'].GMDai.abi;
+const GMDaiBytecode =
+    GMDai.contracts['GMDai.sol'].GMDai.evm.deployedBytecode.object;
+const DaiAddress = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
 
 chai.configure();
 const { expect } = chai;
@@ -15,11 +22,13 @@ if (process.env.NODE_ENV == 'testing') {
     wsHost = 'docker.for.mac.' + wsHost;
 }
 
+// TODO: Move this to a fixture
 const server = ganache.server({
     mnemonic:
         'note flavor live ripple hold salute future drum robot book captain acoustic',
     total_accounts: 10,
     debug: true,
+    // fork: <infura-mainnet-url>
 });
 const port = 8545;
 const provider = `ws://${wsHost}:${port}`;
@@ -97,8 +106,28 @@ describe('gm', () => {
             );
             await gm['web3'].eth.personal.lockAccount(account, console.log);
             expect(await accountIsLocked(gm['web3'], account)).to.be.true;
-            await gm.unlockAccount(account, password);
+            await gm.unlockAccount(account);
             expect(await accountIsLocked(gm['web3'], account)).to.be.false;
+            await gm.close();
+        });
+    });
+
+    describe('#execute()', () => {
+        it('should execute', async () => {
+            await gm.open();
+            const method = 'mint';
+            const from = gm.txSender;
+            const args = [from, 100];
+            const success = await gm.execute(
+                DaiAddress,
+                method,
+                GMDaiAbi,
+                GMDaiBytecode,
+                from,
+                args
+            );
+            expect(success).to.be.true;
+            // TODO: test the balance
             await gm.close();
         });
     });
